8de4f497857c4b95d1305b94bae857c07e10e3d7
YoussefSedekPasha <45726395+YoussefPasha@users.noreply.github.com>
2020-09-21 10:35:19 +0200

    Annonce done

---
+++ src/Home.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Home.css";
+function Home() {
+  return (
+    <div className="home">
+      <div className="home__container">
+        <img
+          className="home__image"
+          src="https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/May/Hero/Fuji_TallHero_Computers_1x._CB432469755_.jpg"
+          alt="AmazonBAr"
+        />
+        <div>
+          
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
