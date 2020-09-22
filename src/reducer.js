295b642a9ae884ff5212f19f566551caca873d4e
YoussefSedekPasha <45726395+YoussefPasha@users.noreply.github.com>
2020-09-22 16:05:59 +0200

    update on Delete checkout product which have some functionalty error

--- src/reducer.js
+++ src/reducer.js
@@ -13,9 +13,19 @@ const reducer = (state, action) => {
         basket: [...state.basket, action.item],
       };
     case "REMOVE_TO_BASKET":
+      const index = state.basket.findIndex(
+        (basketItem) => basketItem.id === action.id
+      );
+      let newBasket = [...state.basket];
+
+      if (index >= 0) {
+        newBasket.splice(index, 1);
+      } else {
+        console.warn(`can't rempve this ${action.id}`);
+      }
       return {
         ...state,
-        basket: state.basket.filter((item) => item.id !== action.id),
+        basket: newBasket,
       };
     default:
       return state;
