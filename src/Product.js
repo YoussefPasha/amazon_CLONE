42aa2dda1622bfd276cc9d31e2833b71fbfadde2
YoussefSedekPasha <45726395+YoussefPasha@users.noreply.github.com>
2020-09-22 11:00:37 +0200

    dispatch the item into the data layer

--- src/Product.js
+++ src/Product.js
@@ -1,6 +1,23 @@
 import React from "react";
 import "./Product.css";
-function Product({ title, image, price, rating }) {
+import { useStateValue } from "./StateProvider";
+
+
+function Product({id, title, image, price, rating }) {
+  const [{ basket }, dispatch] = useStateValue();
+
+  const addToBasket = () => {
+    dispatch({
+      type: "ADD_TO_BASKET",
+      item: {
+        id: id,
+        title: title,
+        image: image,
+        price: price,
+        rating: rating,
+      },
+    });
+  };
   return (
     <div className="product">
       <div className="product__info">
@@ -17,11 +34,8 @@ function Product({ title, image, price, rating }) {
             ))}
         </div>
       </div>
-      <img
-        src={image}
-        alt=""
-      ></img>
-      <button>Add to Basket</button>
+      <img src={image} alt=""></img>
+      <button onClick={addToBasket}>Add to Basket</button>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { StateProvider } from "./StateProvider";
-import initialState from './reducer'
+import {initialState} from "./reducer";
 import reducer from "./reducer";
 ReactDOM.render(
   <React.StrictMode>
