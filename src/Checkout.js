c47fab75768db0ffdd45be5d7b97067c13adc328
YoussefSedekPasha <45726395+YoussefPasha@users.noreply.github.com>
2020-09-23 10:49:38 +0200

    DEMO IS DONE WAIT FOR PAYMENT METHODS

--- src/Checkout.js
+++ src/Checkout.js
@@ -2,9 +2,9 @@ import React from "react";
 import "./Checkout.css";
 import CheckoutProduct from "./CheckoutProduct";
 import { useStateValue } from "./StateProvider";
-import Subtotal from './Subtotal'
+import Subtotal from "./Subtotal";
 function Checkout() {
-  const [{ basket }, dispatch] = useStateValue();
+  const [{ basket, user }, dispatch] = useStateValue();
   return (
     <div className="checkout">
       <div className="checkout__left">
@@ -14,8 +14,9 @@ function Checkout() {
           alt=""
         />
         <div>
+          <h3> Hello , {user?.email.replace("@gmail.com", "")}</h3>
           <h2 className="checkout__title">Your Shoppinh Basket</h2>
-          {basket.map(item => (
+          {basket.map((item) => (
             <CheckoutProduct
               id={item.id}
               title={item.title}
@@ -27,7 +28,7 @@ function Checkout() {
         </div>
       </div>
       <div className="checkout__right">
-        <Subtotal/>
+        <Subtotal />
       </div>
     </div>
   );
