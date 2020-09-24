45000bb9acb6d6a70e9f9a0b56c193dc161d2b69
YoussefSedekPasha <45726395+YoussefPasha@users.noreply.github.com>
2020-09-24 10:56:05 +0200

    import firebase functions and express and cors with api stripe key

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,11 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const express = require("express");
+
+const cors = require("cors");
+
+// hi people use this link to sending money not to fuck me
+
+const stripe = require("stripe")(
+  "sk_test_51HUbuHJpKggh098dnJ2SOsTUtelvr5HTjOUINXdelYPa6d40Fc8ukpPjeV4fMe406Bn7JQqYLCopMqROROnpSzTa00MF0dCWF2"
+);
